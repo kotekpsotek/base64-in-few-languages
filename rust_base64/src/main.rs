1f98159c0156f34feb406d7deda02322f258b6c1
Michał Szczepański <siematumichael@protonmail.com>
2023-02-04 14:07:36 +0100

    Decoding in rust

--- rust_base64/src/main.rs
+++ rust_base64/src/main.rs
@@ -1,4 +1,6 @@
 use std::io;
+use std::str;
+use std::time::{ Instant };
 use base64alphabet::ASCII_BASE64;
 
 mod base64alphabet;
@@ -84,24 +86,112 @@ fn encode(mut sen: String) -> String {
     ready_encoded
 }
 
+/// Decoding base64 to utf-8
 fn decode(mut d_sen: String) -> String {
+    // Remove paddings
+    d_sen = d_sen.replace("=", "");
+
+    // Create binary chain in string
+    let mut as_6bites = String::new();
+    let d_s_s = d_sen.split("").filter(|v| v.len() > 0).collect::<Vec<_>>();
     
+    for char in d_s_s {
+        let char_num_b64alph = ASCII_BASE64.iter()
+            .find(|(_, c)| c.to_string() == char)
+            .unwrap().0;
+        let char_binary = format!("{char_num_b64alph:06b}");
+        as_6bites.push_str(&char_binary)
+    };
+
+    // Create 8 characters chunks
+    let b8_chunks = as_6bites
+        .split("")
+        .filter(|v| v.len() > 0)
+        .collect::<Vec<_>>();
+    let b8_chunks = b8_chunks.chunks(8)
+        .collect::<Vec<_>>();
+
+    // Decode characters from base64 to utf-8 again
+    let mut result = String::new();
+    for chunk in b8_chunks {
+        // Decoding operation = obtain utf-8 character clasified under base64 character
+        let mut op = |chunk: String| {
+            let chunk_byte = u8::from_str_radix(&chunk, 2).unwrap();
+            let ch = String::from_utf8(vec![chunk_byte]).unwrap();
+            result.push_str(&ch)
+        };
+
+        // Perform specific operation to meeted condition
+        if chunk.len() == 8 {
+            // Merge as string chain
+            let chunk = chunk.join("");
+            
+            // Decode operation
+            op(chunk)
+        } else if chunk.contains(&"1") {
+            // Obtain set to refill chain tail with lacking zeros (0)
+            let lacking_zeros = {
+                let mut res: Vec<&str> = vec![];
+                let c = 8 - chunk.len();
+                for _ in 0..c {
+                    res.push("0");
+                }
+                res
+            };
+
+            // Merge as string chain which has got 8 characters (at the tail are 0)
+            let chunk = vec![chunk.to_vec(), lacking_zeros].concat().join("");
+
+            // Decode operation
+            op(chunk);
+        } 
+        // (else) if consist from 0's and is not equal to 8 bytes ignore forth operation
+    }
+
+    result
 }
 
 fn main() {
     let mut buff = String::new();
     io::stdin().read_line(&mut buff).unwrap();
 
-    // println!("{}", 0b00000011u8);
+    // Measure time required to perform both: encoding and decoding
+    let whole_cycle = Instant::now();
+
+    // Encoding
+    // Measure time required to perform encoding
+    let encoding_t = Instant::now();
     let encoded = encode(buff);
-    println!("{encoded}")
+    println!("Encoded: {encoded}");
+    println!("Encoding operation takes: {}ns", encoding_t.elapsed().as_nanos());
+
+    // Decoding
+    // Measure time required to perform decoding
+    let decoding_t = Instant::now();
+    let decoded = decode(encoded);
+    println!("Decoded: {decoded}");
+    println!("Decoding operation takes: {}ns", decoding_t.elapsed().as_nanos());
+
+    println!("Whole circle takes: {}ns", whole_cycle.elapsed().as_nanos());
 }
 
 #[test]
 fn encoding_test() {
-    let mut buff = String::new();
-    io::stdin().read_line(&mut buff).unwrap();
+    let ex = String::from("Example to encode");
 
-    let encoded = encode(buff);
-    println!("{encoded}")
+    // Encoding
+    let encoded = encode(ex);
+    println!("Encoded: {encoded}")
+}
+
+#[test]
+fn decoding_test() {
+    let ex = String::from("Example to encode and decode");
+
+    // Must be firstly encoded to check decoding
+    let e_ = encode(ex);
+
+    // Decoding prior encoded
+    let decoded = decode(e_);
+    println!("Decoded: {decoded}");
 }
